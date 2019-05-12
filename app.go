34278c96dbf3011ec4d1808336fda83e000b5579
David whyte <kingstonwhyte87@gmail.com>
2019-05-12 09:40:55 +0100

    init

---
+++ app.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+  print("my app!\n")
+  print(max(2, 5))
+}
+
+func max(num1, num2 int) int {
+  if (num1>num1) {
+    return num1
+  } else {
+    return num2
+  }
+}
